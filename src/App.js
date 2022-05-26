d3698558d9bed6973d1bad41016fc48f698a5bb6
Gloria Uloaku <tariak2@hotmail.com>
2022-05-26 18:07:51 +0100

    4th commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,62 @@
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
 import NotesList from './components/NotesList';
 
-   const App = () => {
+   const App = () => { 
+     const [notes, setNotes] = useState([
+       {
+       id:nanoid(),
+       text: 'This is my 1st note!!',
+       date: '26/05/2022',
+       },
+
+       {
+       id:nanoid(),
+       text: 'This is my 2nd note!!',
+       date: '27/05/2022',
+       },
+
+       {
+       id:nanoid(),
+       text: 'This is my 3rd note!!',
+       date: '28/05/2022',
+       },
+
+       {
+       id:nanoid(),
+       text: 'This is my 4th note!!',
+       date: '29/05/2022',
+       },
+
+       {
+       id:nanoid(),
+       text: 'This is my 5th note!!',
+       date: '30/05/2022',
+       },
+      
+       {
+       id:nanoid(),
+       text: 'This is my 6th note!!',
+       date: '31/05/2022',
+       },
+
+       {
+       id:nanoid(),
+       text: 'This is my 7th note!!',
+       date: '01/06/2022',
+       },
+
+       {
+       id:nanoid(),
+       text: 'This is my 8th note!!',
+       date: '2/06/2022',
+       },
+
+     ]);
+
      return (
         <div className='container'>
-            <NotesList />
+            <NotesList  notes={notes}/>
         </div>
      );
     
